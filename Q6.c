35911d45c171af57ecf7d5daf5b541aa2493fbeb
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-09-30 15:07:26 +0530

    Add files via upload

---
+++ Q6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int number=24;
+     if(number % 3 == 0 && number % 4 == 0){
+printf("Divisible by both 3 and 4\n");
+     }else{
+        printf("Not divisible by both 3 and 4\n");
+
+     }
+     
+     return 0;
+     
+}
