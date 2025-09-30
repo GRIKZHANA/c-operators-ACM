26e5f643e92986193c3ce481fba4e27e6973352e
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-09-30 15:06:28 +0530

    Add files via upload

---
+++ Q5.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    char password =100;
+    int requiredlength=8;
+    int enteredpassword=12;
+    printf("Enter your password:%d\n",enteredpassword<requiredlength);
+    return 0;
+
+}
