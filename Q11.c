0c1a52219e2677f0908e0a704dbd8563049e5ae6
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-10-25 18:58:11 +0530

    Add files via upload

---
+++ Q11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int attendance = 70;
+    int marks = 85;
+
+    if (attendance >= 75 && marks >= 70) {
+        printf("The student qualifies.\n");
+    } else {
+        printf("The student does not qualify.\n");
+    }
+
+    return 0;
+}
