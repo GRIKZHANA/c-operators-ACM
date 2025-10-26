86d68d2571f2436bd7b8e562be378347fa1115db
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-10-26 16:17:37 +0530

    Add files via upload

---
+++ Q14.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int day = 7; // Today is day 7
+
+    if (day >= 1 && day <= 5) {
+        printf("The shop is open.\n");
+    } else {
+        printf("The shop is closed.\n");
+    }
+
+    return 0;
+}
