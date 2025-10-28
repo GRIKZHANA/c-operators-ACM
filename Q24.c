7275b069023470b96e8b73c8d8424dde7ab758c9
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-10-28 18:25:55 +0530

    Add files via upload

---
+++ Q24.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int root = sqrt(num);  // find integer square root
+
+    if (root * root == num) {
+        printf("%d is a perfect square.\n", num);
+    } else {
+        printf("%d is not a perfect square.\n", num);
+    }
+
+    return 0;
+}
