b12cbfc7e58e4540029735a6e591f9f720a94e02
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-11-12 01:36:48 +0530

    Add files via upload

---
+++ Question9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0, digit;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    while(num != 0) {
+        digit = num % 10;   // Get last digit
+        sum += digit;        // Add to sum
+        num = num / 10;      // Remove last digit
+    }
+
+    printf("Sum of digits: %d\n", sum);
+
+    return 0;
+}
